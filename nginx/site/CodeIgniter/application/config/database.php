8d9e53bff3476f7b0cd1a130b330faa19429a9b9
Ayuth Mangmesap <ayuth.m@thaistg.com>
2017-12-09 08:33:34 +0700

    config Codeigniter to connect to our mysql

--- nginx/site/CodeIgniter/application/config/database.php
+++ nginx/site/CodeIgniter/application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
+	'hostname' => '172.20.1.4',
+	'username' => 'root',
+	'password' => 'asdf',
 	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
